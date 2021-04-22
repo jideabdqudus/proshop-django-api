affc13e475de3581af1cf600db56ec968acae052
jideabdqudus <jideabdqudus@gmail.com>
2021-04-22 22:13:25 +0100

    +

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'base.apps.BaseConfig',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -125,3 +130,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
+    "http://127.0.0.1:3000"
+]
---
+++ backend/base/products.py
@@ -0,0 +1,38 @@
+products = [
+    {
+        '_id': '1',
+        'name': 'Apple Wireless Bluetooth Headphones',
+        'image': '/images/airpods.jpg',
+        'description': 'Bluetooth technology lets you connect with compatible devices',
+        'brand': 'Apple',
+        'category': 'Electronics',
+        'price': 89.99,
+        'countInStock': 10,
+        'rating': '4.5',
+        'numReviews': 12
+    },
+    {
+        '_id': '2',
+        'name': 'Apple iPhone 11 PRO',
+        'image': '/images/airpods.jpg',
+        'description': 'Mobile smartphone for your use',
+        'brand': 'Apple',
+        'category': 'Electronics',
+        'price': 289.99,
+        'countInStock': 8,
+        'rating': '4.3',
+        'numReviews': 16
+    },
+    {
+        '_id': '3',
+        'name': 'Apple MacBook',
+        'image': '/images/airpods.jpg',
+        'description': 'Computer for your use',
+        'brand': 'Apple',
+        'category': 'Electronics',
+        'price': 659.99,
+        'countInStock': 3,
+        'rating': '4.8',
+        'numReviews': 5
+    }
+]
